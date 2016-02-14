15e08530b4f73c8433e6777287c86ca348ea4d4b
Björn Norgren <bjonor@gmail.com>
2016-02-14 17:27:21 +0100

    Add a handfull of tests for the Zelus module

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
+import qualified TestZelus
+
+-- | Runs the test suite
 main :: IO ()
-main = putStrLn "undefined"
+main =  defaultMain $ testGroup "Test suite" [TestZelus.tests]
---
+++ test/TestZelus.hs
@@ -0,0 +1,32 @@
+module TestZelus where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import Test.QuickCheck
+
+import Zelus
+
+tests :: TestTree
+tests = testGroup "Zelus"
+  [ testProperty "Up and down not true at the same time" prop_notUpAndDown
+  , testProperty "Advance and delay a stream equals the original" prop_nextPre
+  , testProperty "Derivative of linear function is constant" prop_linearDeriv
+  , testProperty "Derivative of square function is increasing" prop_squareDeriv
+  ]
+
+prop_notUpAndDown :: S Int -> Bool
+prop_notUpAndDown x = not . or $ (up x) &&? (down x)
+
+prop_nextPre :: S Int -> Property
+prop_nextPre x = not (null x) ==> and $ next (pre x) ==? x
+
+prop_linearDeriv :: Int -> Positive Int -> Bool
+prop_linearDeriv dx (Positive len) = and $ dxs ==? next dxs
+  where
+    dxs = deriv $ map (*dx) [(-len)..len]
+
+prop_squareDeriv :: Positive Int -> Positive Int -> Bool
+prop_squareDeriv (Positive dx) (Positive len) = and $ dxs <? next dxs
+  where
+    dxs = deriv $ [x*x | x <- [(-len*dx), dx .. len*dx]]
