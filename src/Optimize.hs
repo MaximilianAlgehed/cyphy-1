1d2cf979ec274d3f81f7094c030ab0ddccaa5055
Koen Claessen <koen@chalmers.se>
2016-02-18 11:55:04 +0100

    more sane optimization (I think)

--- src/Optimize.hs
+++ src/Optimize.hs
@@ -17,7 +17,7 @@ opti f (x0,x1) = search x0 (f x0) x1 (f x1)
     | ma >= mb  = search x0 y0 xb yb
     | otherwise = search xa ya x1 y1
    where
-    k    = 6
+    k    = 10
     t    = 0.33 / fromIntegral k
     xyas = (x0,y0) : take k [ (x,f x) | d <- [t,t+t..], let x = (1-d)*x0 + d*x1 ]
     xybs = (x1,y1) : take k [ (x,f x) | d <- [t,t+t..], let x = d*x0 + (1-d)*x1 ]
@@ -31,26 +31,29 @@ opti f (x0,x1) = search x0 (f x0) x1 (f x1)
 optiVec :: ([Double] -> Double) -> ([Double],[Double]) -> [Double]
 optiVec f (v0,v1)
   | n /= length v1 = error "optiVec: vector lengths do not match"
-  | otherwise      = improving v
+  | otherwise      = improving e v
  where
   n = length v0
   v = zipWith avg v0 v1
+  e = zipWith (-) v1 v0
 
   x `avg` y = (x+y) / 2
 
-  improving v
-    | f v' > f v = improving v'
+  improving e v
+    | f v' > f v = improving (map (*0.95) e) v'
     | otherwise  = v
    where
-    v' = improve v
+    v' = improve e v
 
-  improve v = fst $ maximumBy (comparing snd) $ [ (v, f v) | v <- vs ]
+  improve e v = fst $ maximumBy (comparing snd) $ [ (v, f v) | v <- vs ]
    where
-    vs = nub $ concat
-         [ [ v =!! (i, opti (\x -> f (v =!! (i,x))) (v0!!i,v !!i))
-           , v =!! (i, opti (\x -> f (v =!! (i,x))) (v !!i,v1!!i))
-           ]
+    vs = nub 
+         [ v =!! (i, opti (\x -> f (v =!! (i,x))) (v0!!i,v!!i))
          | i <- [0..n-1]
+         , let ex = (e!!i) / 2
+               x0 = xm-ex
+               xm = v!!i
+               x1 = xm+ex
          ]
 
   (=!!) :: [a] -> (Int,a) -> [a]
