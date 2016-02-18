c7d8a3a595746bb68ac7cafa94e11363bd919257
Björn Norgren <bjonor@gmail.com>
2016-02-18 22:55:37 +0100

    Add ugly controller

--- src/ACAS.hs
+++ src/ACAS.hs
@@ -2,6 +2,36 @@ module ACAS where
 
 import Zelus
 
+-- | Aircraft collision avoidance system controller.
+acas :: S Double -- ^ Distance to offending aircraft.
+     -> S Double -- ^ Resulting course correction.
+acas = inactive
+
+-- | Inactive mode, no course correction.
+inactive :: S Double -- ^ Distance to offending aircraft.
+         -> S Double -- ^ Resulting course correction.
+inactive (d:ds) =
+    if d < r
+    then right : active ds 0
+    else omega : inactive ds
+  where
+    right = pi/2
+    omega = 0.0
+    r = 5
+
+-- | Active mode, yes course correction.
+active :: S Double -- ^ Distance to offending aircraft.
+       -> Double
+       -> S Double -- ^ Resulting course correction.
+active (d:ds) acc =
+    if acc > pi
+    then left : inactive ds
+    else omega : active ds (acc+omega)
+  where
+    left  = -pi/2
+    omega = 0.01
+    z     = integ $ (val omega) `in1t` 0
+
 -- | Plant modelling the aircraft.
 aircraft :: S Double                   -- ^ Course correction.
          -> S Double                   -- ^ Disturbance along x axis.
@@ -18,3 +48,20 @@ aircraft thetadot dx dy = zip3 x y theta
     ydot      = initv * sin theta + dy
     x         = integ $ xdot `in1t` initx
     y         = integ $ ydot `in1t` inity
+
+
+{-  where
+    rght       = val (pi/2)
+    lft        = -rght
+    r          = val 20
+    omegadot1  = 0.0
+    omegadot2  = 0.1
+    zdot1      = 0.0
+    zdot2      = 1.0
+    mode1      = val (omegadot1, zdot1)
+    mode2      = val (omegadot2, zdot2)
+    triggered  = d <? r
+    state      = triggered ? (mode2, mode1)
+    correction = undefined
+    hm         = undefined
+-}
