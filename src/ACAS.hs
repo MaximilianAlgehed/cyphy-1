0c5f68c295a5c5be49b41213739d64552a067c06
Björn Norgren <bjonor@gmail.com>
2016-02-23 13:02:38 +0100

    Reimplement controller using automaton

--- src/ACAS.hs
+++ src/ACAS.hs
@@ -2,48 +2,48 @@ module ACAS where
 
 import Zelus
 
+data Mode = Active | Inactive deriving (Eq, Show)
+
+tscale :: S Double -> S Double
+tscale t = 0.001 * t
+
 -- | Aircraft collision avoidance system controller.
-acas :: S Double -- ^ Distance to offending aircraft.
+acas :: S Bool   -- ^ Avoidance trigger.
      -> S Double -- ^ Resulting course correction.
-acas = inactive
-
--- | Inactive mode, no course correction.
-inactive :: S Double -- ^ Distance to offending aircraft.
-         -> S Double -- ^ Resulting course correction.
-inactive (d:ds) =
-    if d < r
-    then right : active ds 0
-    else omega : inactive ds
+acas trigger = correction
   where
-    right = pi/2
-    omega = 0.0
-    r = 5
+    omega, left :: S Double
+    omega = tscale 1
+    left  = -pi / 2
 
--- | Active mode, yes course correction.
-active :: S Double -- ^ Distance to offending aircraft.
-       -> Double   -- ^ Arc integrator.
-       -> S Double -- ^ Resulting course correction.
-active (d:ds) acc =
-    if acc > pi
-    then right : inactive ds
-    else -omega : active ds (acc+omega)
-  where
-    right = pi/2
-    omega = 0.01
+    zdot, z :: S Double
+    zdot = omega
+    z    = integ (zdot `in1t` 0 `reset` (0 `when` (mode ==? val Inactive)))
+
+    mode :: S Mode
+    mode = automaton
+      [ Inactive >-- trigger    --> Active
+      , Active   >-- (z >=? pi) --> Inactive
+      ]
+
+    correction :: S Double
+    correction = enterCorrect + correct + leaveCorrect
+      where
+        enterCorrect = mode `took` (Inactive --> Active) ? (left, 0)
+        correct      = mode ==? val Active ? (omega, 0)
+        leaveCorrect = mode `took` (Active --> Inactive) ? (left, 0)
 
 -- | Plant modelling the aircraft.
-aircraft :: S Double                   -- ^ Course correction.
-         -> S Double                   -- ^ Disturbance along x axis.
-         -> S Double                   -- ^ Disturbance along y axis.
-         -> S (Double, Double, Double) -- ^ Resulting position and heading.
-aircraft thetadot dx dy = zip3 x y theta
+aircraft :: (Double, Double, Double) -- ^ Initial position and heading.
+         -> S Double                 -- ^ Disturbance along x axis.
+         -> S Double                 -- ^ Disturbance along y axis.
+         -> S Double                 -- ^ Course correction.
+         -> (S Double, S Double)     -- ^ Resulting position.
+aircraft (initx, inity, inittheta) dx dy thetadot = (x, y)
   where
-    inittheta = pi/4
-    initx     = 10
-    inity     = 10
-    v         = val 4
-    theta     = integ $ thetadot `in1t` inittheta
+    v         = tscale 100
+    theta     = integ $ thetadot `in1t` val inittheta
     xdot      = v * cos theta + dx
     ydot      = v * sin theta + dy
-    x         = integ $ xdot `in1t` initx
-    y         = integ $ ydot `in1t` inity
+    x         = integ $ xdot `in1t` val initx
+    y         = integ $ ydot `in1t` val inity
