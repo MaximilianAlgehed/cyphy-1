1c1d2e4eeb342daded7193ebcb27c55101cdeb83
Björn Norgren <bjonor@gmail.com>
2016-04-07 16:50:51 +0200

    Start work on test of railroad crossing model

---
+++ src/TestRC.hs
@@ -0,0 +1,43 @@
+module TestRC where
+
+import Test.QuickCheck
+import Zelus
+import RailroadCrossing
+
+safetyDistance = 250
+steps = 3500
+ddStep = 0.5
+dxStep = 0.05
+
+valid (S ddA ddN cS) =
+  -45 >= ddA && ddA >= -55 && -35 >= ddN && ddN >= -50 && 0.9 <= cS && cS <= 1.1
+
+data Speeds = S Double Double Double deriving (Eq, Show)
+
+instance Arbitrary Speeds where
+  arbitrary =
+    do
+      ddApproach <- choose (-45, -55)
+      ddNear <- choose (-35, -50)
+      closingSpeed <- choose (0.9, 1.1)
+      return (S ddApproach ddNear closingSpeed)
+
+  shrink (S ddA ddN cS) =
+    let
+      ddA' = ddA + ddStep
+      ddN' = ddN + ddStep
+      cS' = cS + dxStep
+    in reverse $ filter valid
+      [ S ddA ddN cS'
+      , S ddA ddN' cS
+      , S ddA ddN' cS'
+      , S ddA' ddN cS
+      , S ddA' ddN cS'
+      , S ddA' ddN' cS
+      , S ddA' ddN' cS'
+      ]
+
+prop_fail (S ddApproach ddNear closingSpeed) = not . or $ take steps $ map failed result
+  where
+    result = run ddApproach ddNear closingSpeed safetyDistance
+    failed (d, g) = abs d <= 10 && g /= Closed
