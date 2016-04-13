612b3ccaf21e3988ba7c6642ca1e9c4aec3d99a3
Björn Norgren <bjonor@gmail.com>
2016-04-13 04:12:34 +0200

    add composite statistic to test cases

--- src/TestRC_QC.hs
+++ src/TestRC_QC.hs
@@ -9,10 +9,10 @@ steps = 3500
 ddStep = 0.5
 dxStep = 0.05
 
-valid (S ddA ddN cS) =
+valid (S ddA ddN cS _) =
   -45 >= ddA && ddA >= -55 && -35 >= ddN && ddN >= -50 && 0.9 <= cS && cS <= 1.1
 
-data Speeds = S Double Double Double deriving (Eq, Show)
+data Speeds = S Double Double Double Double deriving (Eq, Show)
 
 instance Arbitrary Speeds where
   arbitrary =
@@ -20,24 +20,28 @@ instance Arbitrary Speeds where
       ddApproach <- choose (-45, -55)
       ddNear <- choose (-35, -50)
       closingSpeed <- choose (0.9, 1.1)
-      return (S ddApproach ddNear closingSpeed)
+      let composite = (ddApproach + ddNear) / closingSpeed
+      return (S ddApproach ddNear closingSpeed composite)
 
-  shrink (S ddA ddN cS) =
+  shrink (S ddA ddN cS _) =
     let
       ddA' = ddA + ddStep
       ddN' = ddN + ddStep
       cS' = cS + dxStep
+      com = (ddA' + ddN') / cS'
     in reverse $ filter valid
-      [ S ddA ddN cS'
-      , S ddA ddN' cS
-      , S ddA ddN' cS'
-      , S ddA' ddN cS
-      , S ddA' ddN cS'
-      , S ddA' ddN' cS
-      , S ddA' ddN' cS'
+      [ S ddA ddN cS' com
+      , S ddA ddN' cS com
+      , S ddA ddN' cS' com
+      , S ddA' ddN cS com
+      , S ddA' ddN cS' com
+      , S ddA' ddN' cS com
+      , S ddA' ddN' cS' com
       ]
 
-prop_fail (S ddApproach ddNear closingSpeed) = not . or $ take steps $ map failed result
+prop_fail :: Speeds -> Bool
+prop_fail (S ddApproach ddNear closingSpeed _) =
+  not . or $ take steps $ map failed result
   where
     result = run ddApproach ddNear closingSpeed safetyDistance
     failed (d, g) = abs d <= 10 && g /= Closed
