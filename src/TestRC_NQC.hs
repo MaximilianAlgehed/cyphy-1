f7b7e4f7b000cc9e461108c300ba9afcc6d5e31c
Björn Norgren <bjonor@gmail.com>
2016-04-14 20:22:16 +0200

    remove test case statistic

--- src/TestRC_NQC.hs
+++ src/TestRC_NQC.hs
@@ -9,10 +9,10 @@ steps = 3500
 ddStep = 0.5
 dxStep = 0.05
 
-valid (S ddA ddN cS _) =
+valid (S ddA ddN cS) =
   -45 >= ddA && ddA >= -55 && -35 >= ddN && ddN >= -50 && 0.9 <= cS && cS <= 1.1
 
-data Speeds = S Double Double Double Double deriving (Eq, Show)
+data Speeds = S Double Double Double deriving (Eq, Show)
 
 instance Arbitrary Speeds where
   arbitrary =
@@ -20,31 +20,30 @@ instance Arbitrary Speeds where
       ddApproach <- choose (-45, -55)
       ddNear <- choose (-35, -50)
       closingSpeed <- choose (0.9, 1.1)
-      let composite = (ddApproach + ddNear) / closingSpeed
-      return (S ddApproach ddNear closingSpeed composite)
+      return (S ddApproach ddNear closingSpeed)
 
-  shrink (S ddA ddN cS _) =
+  shrink (S ddA ddN cS) =
     let
       ddA' = ddA + ddStep
       ddN' = ddN + ddStep
       cS' = cS + dxStep
-      com = (ddA' + ddN') / cS'
     in reverse $ filter valid
-      [ S ddA ddN cS' com
-      , S ddA ddN' cS com
-      , S ddA ddN' cS' com
-      , S ddA' ddN cS com
-      , S ddA' ddN cS' com
-      , S ddA' ddN' cS com
-      , S ddA' ddN' cS' com
+      [ S ddA ddN cS'
+      , S ddA ddN' cS
+      , S ddA ddN' cS'
+      , S ddA' ddN cS
+      , S ddA' ddN cS'
+      , S ddA' ddN' cS
+      , S ddA' ddN' cS'
       ]
 
 prop_fail :: Speeds -> Double
-prop_fail (S ddApproach ddNear closingSpeed _) =
+prop_fail (S ddApproach ddNear closingSpeed) =
   minimum $  -- instead of `and`ing the list together; find the worst value
     take steps $ map failed result
   where
     result = run ddApproach ddNear closingSpeed safetyDistance
-    failed (d, g) = if abs d <= 10 && g /= Closed -- fail condition
-                    then abs d - 10 -- badness value, distance from the crossing
-                    else 0 -- non-negative is true
+    failed (d, g) = if abs d <= 10 && g /= Closed then -(d + 10) else 0
+                    --if abs d <= 10 && g /= Closed -- fail condition
+                    --then abs d - 10 -- badness value, distance from the crossing
+                    --else 0 -- non-negative is true
--- src/TestRC_QC.hs
+++ src/TestRC_QC.hs
@@ -9,10 +9,10 @@ steps = 3500
 ddStep = 0.5
 dxStep = 0.05
 
-valid (S ddA ddN cS _) =
+valid (S ddA ddN cS) =
   -45 >= ddA && ddA >= -55 && -35 >= ddN && ddN >= -50 && 0.9 <= cS && cS <= 1.1
 
-data Speeds = S Double Double Double Double deriving (Eq, Show)
+data Speeds = S Double Double Double deriving (Eq, Show)
 
 instance Arbitrary Speeds where
   arbitrary =
@@ -20,27 +20,25 @@ instance Arbitrary Speeds where
       ddApproach <- choose (-45, -55)
       ddNear <- choose (-35, -50)
       closingSpeed <- choose (0.9, 1.1)
-      let composite = (ddApproach + ddNear) / closingSpeed
-      return (S ddApproach ddNear closingSpeed composite)
+      return (S ddApproach ddNear closingSpeed)
 
-  shrink (S ddA ddN cS _) =
+  shrink (S ddA ddN cS) =
     let
       ddA' = ddA + ddStep
       ddN' = ddN + ddStep
       cS' = cS + dxStep
-      com = (ddA' + ddN') / cS'
     in reverse $ filter valid
-      [ S ddA ddN cS' com
-      , S ddA ddN' cS com
-      , S ddA ddN' cS' com
-      , S ddA' ddN cS com
-      , S ddA' ddN cS' com
-      , S ddA' ddN' cS com
-      , S ddA' ddN' cS' com
+      [ S ddA ddN cS'
+      , S ddA ddN' cS
+      , S ddA ddN' cS'
+      , S ddA' ddN cS
+      , S ddA' ddN cS'
+      , S ddA' ddN' cS
+      , S ddA' ddN' cS'
       ]
 
 prop_fail :: Speeds -> Bool
-prop_fail (S ddApproach ddNear closingSpeed _) =
+prop_fail (S ddApproach ddNear closingSpeed) =
   not . or $ take steps $ map failed result
   where
     result = run ddApproach ddNear closingSpeed safetyDistance
