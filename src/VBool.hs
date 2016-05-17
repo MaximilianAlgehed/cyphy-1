4c84ba47fe532606c152014217541d1f191d683c
Björn Norgren <bjonor@gmail.com>
2016-05-17 10:38:55 +0200

    some rearrangements

--- src/VBool.hs
+++ src/VBool.hs
@@ -1,38 +1,35 @@
 module VBool where
 
-import Test.QuickCheck
+infixr 1 =>. , =>:
+infixr 2 ||. , ||:
+infixr 3 &&. , &&:
+infix 4 <=. , <. , >. , >=. , ==. , /=.
+infix 4 <=: , <: , >: , >=: , ==: , /=:
 
-newtype VBool = VBool { val :: Double }
+newtype VBool = VBool { truthness :: Double }
  deriving ( Eq, Ord )
 
 instance Show VBool where
-  show = show . val
+  show = ("B:"++) . show . truthness
+
+
 
 false, true :: VBool
 false = VBool (-1)
 true  = VBool 1
 
-vbFromBool :: Bool -> VBool
-vbFromBool b = if b then true else false
+boolify :: VBool -> Bool
+boolify = (>=0) . truthness
 
-vbFromDouble :: Double -> VBool
-vbFromDouble = VBool
 
-vbool :: Bool -> VBool
-vbool b = if b then true else false
 
 nt :: VBool -> VBool
 nt (VBool d) = VBool (-d)
 
-(#) :: Double -> VBool -> VBool
-a # (VBool d) | a > 0 = VBool (a*d)
-
-(&&.), (||.), (=>.), (<=>.) :: VBool -> VBool -> VBool
+(&&.), (||.), (=>.) :: VBool -> VBool -> VBool
 VBool a &&. VBool b = VBool (a `min` b)
 VBool a ||. VBool b = VBool (a `max` b)
-
 p =>.  q = nt p ||. q
-p <=>. q = (p =>. q) &&. (q =>. p) -- does this even make sense?
 
 (<=.), (<.), (>.), (>=.), (==.), (/=.) :: Double -> Double -> VBool
 a <=. b | a <= b    = VBool (1 + b-a)
@@ -44,9 +41,20 @@ a ==. b | a == b    = VBool 1
         | otherwise = VBool (-1 - abs (a-b))
 a /=. b = nt (a ==. b)
 
--- Changed definition so zero is counted as true
-isTrue :: VBool -> Bool
-isTrue = (>=0) . val
 
-trueness :: VBool -> Double
-trueness = val
+
+nts :: [VBool] -> [VBool]
+nts = map nt
+
+(&&:), (||:), (=>:) :: [VBool] -> [VBool] -> [VBool]
+(&&:) = zipWith (&&.)
+(||:) = zipWith (||.)
+(=>:) = zipWith (=>.)
+
+(<=:), (<:), (>:), (>=:), (==:), (/=:) :: [Double] -> [Double] -> [VBool]
+(<=:) = zipWith (<=.)
+(<:) = zipWith (<.)
+(>:) = zipWith (>.)
+(>=:) = zipWith (>=.)
+(==:) = zipWith (==.)
+(/=:) = zipWith (/=.)
