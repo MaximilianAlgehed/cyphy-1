748f149641a42e04168381868bcb6c5ae6915226
Björn Norgren <bjonor@gmail.com>
2016-05-17 11:20:57 +0200

    rename

--- test/TestCruiseControl.hs
+++ test/TestCruiseControl.hs
@@ -1,5 +1,28 @@
+{-# LANGUAGE ImplicitParams #-}
+
 module TestCruiseControl where
 
-import Zelus
+import Prelude hiding (until)
 
-import CruiseControl
+import Data.List
+
+import Test.QuickCheck
+
+import Zelus
+import CyphyUtils
+import PastLTL
+
+-- Overshoot
+-- always ([ref_up, ref_down) -> v - ref <. 1.02 * ref)
+prop_overshoot = undefined
+  where
+    ref_up = up ref
+    ref_down = down ref
+-- Undershoot
+
+-- Stable regime: not over set speed after 5 seconds
+-- Steady state error < 1km/h
+
+-- Rise time
+-- Från refändring till mellan 98% och 102% av refvärde
+--
