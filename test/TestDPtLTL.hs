723bd51a57df5f7598eac8ba42bfe4bb1a789efb
Björn Norgren <bjonor@gmail.com>
2016-05-18 02:56:21 +0200

    ptltl property tests

--- test/TestDPtLTL.hs
+++ test/TestDPtLTL.hs
@@ -1,20 +1,61 @@
+-- | Testing the Past time LTL.
+-- The article lists and proves 16 properties (p.5) for the PtLTL.
+-- Corresponding tests in the suite below.
+
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module TestDPtLTL where
 
-import Test.QuickCheck
+import Test.Tasty
+import Test.Tasty.QuickCheck hiding (once)
 
-import Zelus ()
 import Dool
+import DPtLTL
 
-import
+instance Arbitrary Dool where
+  arbitrary = elements [true, false]
 
-data Case = Case { ds :: [Dool] } deriving (Show)
+instance Arbitrary [Dool] where
+  arbitrary = listOf1 (frequency [(9, arbitrary), (1, arbitrary)])
 
-instance Arbitrary Case where
-  arbitrary =
-    do xs <- arbitrary
-       return (Case (take 100 (xs >=: 0)))
+prop_1 f = once f == trues `sinces` f
+prop_2 f = always f == nts (once (nts f))
+prop_3 f1 f2 = f1 `sincew` f2 == (always f1 ||: (f1 `sinces` f2))
+prop_4 f = always f == f `sincew` falses
+prop_5 f = once f == nts (always (nts f))
+prop_6 f1 f2 = f1 `sinces` f2 == (once f2 &&: (f1 `sincew` f2))
+prop_7 f = begin f == (f &&: nts (prev f))
+prop_8 f = end f == (nts f &&: prev f)
+prop_9 f1 f2 = f1 `intervals` f2 == (nts f2 &&: (prev (nts f2) `sinces` f1))
+prop_10 f1 f2 = f1 `intervalw` f2 == (nts f2 &&: (prev (nts f2) `sincew` f1))
+prop_11 f = end f == begin (nts f)
+prop_12 f = begin f == end (nts f)
+prop_13 f1 f2 = f1 `intervalw` f2 == (always (nts f2) ||: (f1 `intervals` f2))
+prop_14 f1 f2 = f1 `intervals` f2 == (once f1 &&: (f1 `intervalw` f2))
+prop_15 f = prev f == ((f =>: nts (begin f)) &&: (nts f =>: end f))
+prop_16 f1 f2 = f1 `sinces` f2 == (f2 ||: (prev f2 `intervals` nts f1))
 
+main = defaultMain $
+  testGroup "test dptltl"
+    [ props
+    ]
 
-
-prop_1 :: Case -> Bool
-prop_1 c = once (ds c) == trues `sinces` (ds c)
+props = testGroup "properties"
+  [ testProperty "prop 1" prop_1
+  , testProperty "prop 2" prop_2
+  , testProperty "prop 3" prop_3
+  , testProperty "prop 4" prop_4
+  , testProperty "prop 5" prop_5
+  , testProperty "prop 6" prop_6
+  , testProperty "prop 7" prop_7
+  , testProperty "prop 8" prop_8
+  , testProperty "prop 9" prop_9
+  , testProperty "prop 10" prop_10
+  , testProperty "prop 11" prop_11
+  , testProperty "prop 12" prop_12
+  , testProperty "prop 13" prop_13
+  , testProperty "prop 14" prop_14
+  , testProperty "prop 15" prop_15
+  , testProperty "prop 16" prop_16
+  ]
